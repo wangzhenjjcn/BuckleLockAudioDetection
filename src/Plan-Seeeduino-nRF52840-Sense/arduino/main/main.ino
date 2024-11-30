37228882283bb298e0178b7f063af58bb636b8ab
WangZhen <wangzhenjjcn@gmail.com>
2024-11-30 09:54:51 +0800

    update

--- src/Plan-Seeeduino-nRF52840-Sense/arduino/main/main.ino
+++ src/Plan-Seeeduino-nRF52840-Sense/arduino/main/main.ino
@@ -1,153 +1,97 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
+// The MIT License (MIT)
+// Copyright (c) 2019 Ha Thach for Adafruit Industries
 
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
+#include "SdFat.h"
+#include "Adafruit_SPIFlash.h"
 
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
+// Uncomment to run example with custom SPI and SS e.g with FRAM breakout
+// #define CUSTOM_CS   A5
+// #define CUSTOM_SPI  SPI
 
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
+#if defined(CUSTOM_CS) && defined(CUSTOM_SPI)
+  Adafruit_FlashTransport_SPI flashTransport(CUSTOM_CS, CUSTOM_SPI);
 
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3-5V DC
-   Connect GROUND to common ground
+#elif defined(ARDUINO_ARCH_ESP32)
+  // ESP32 use same flash device that store code.
+  // Therefore there is no need to specify the SPI and SS
+  Adafruit_FlashTransport_ESP32 flashTransport;
 
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
+#else
+  // On-board external flash (QSPI or SPI) macros should already
+  // defined in your board variant if supported
+  // - EXTERNAL_FLASH_USE_QSPI
+  // - EXTERNAL_FLASH_USE_CS/EXTERNAL_FLASH_USE_SPI
+  #if defined(EXTERNAL_FLASH_USE_QSPI)
+    Adafruit_FlashTransport_QSPI flashTransport;
 
-/* Set the delay between fresh samples */
-uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+  #elif defined(EXTERNAL_FLASH_USE_SPI)
+    Adafruit_FlashTransport_SPI flashTransport(EXTERNAL_FLASH_USE_CS, EXTERNAL_FLASH_USE_SPI);
 
-// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
+  #else
+    #error No QSPI/SPI flash are defined on your board variant.h !
+  #endif
+#endif
 
-void setup(void)
+Adafruit_SPIFlash flash(&flashTransport);
+
+
+/*  If you want to use a specific flash device, for example for a custom built board, first look for it in Adafruit_SPIFlash\src\flash_devices.h
+ *  If it isn't in there you need to create your own definition like the W25Q80DLX_EXAMPLE example below.
+ *  These definitions need to be edited to match information on the data sheet of the flash device that you want to use.
+ *  If you are not sure what the manufacture ID, memory type and capacity values should be, try running the sketch anyway and look at the serial output
+ *  The flash device will report these values to you as a single hexadecimal value (the JDEC ID)
+ *  For example, the first device on the list - the W25Q80DLX - will report its JDEC ID as 0xef4014, which is made of these three values:
+ *  manufacturer_id = 0xef
+ *  memory_type     = 0x40
+ *  capacity        = 0x14
+ *  With this macro properly defined you can then create an array of device definitions as shown below, this can include any from the list of devices in flash_devices.h, and any you define yourself here
+ *  You need to update the variable on line 71 to reflect the number of items in the array
+ *  You also need to uncomment line 84 and comment out line 81 so this array will be passed to the flash memory driver. 
+ */
+//Example of a user defined flash memory device:
+//#define W25Q80DLX_EXAMPLE                                                               \
+//  {                                                                            \
+//    .total_size = (1 << 20), /* 1 MiB */                                       \
+//        .start_up_time_us = 5000, .manufacturer_id = 0xef,                     \
+//    .memory_type = 0x40, .capacity = 0x14, .max_clock_speed_mhz = 80,         \
+//    .quad_enable_bit_mask = 0x02, .has_sector_protection = false,              \
+//    .supports_fast_read = true, .supports_qspi = true,                         \
+//    .supports_qspi_writes = false, .write_status_register_split = false,       \
+//    .single_status_byte = false, .is_fram = false,                             \
+//  }
+
+/*
+ * Create an array of data structures and fill it with the settings we defined above.
+ * We are using two devices, but more can be added if you want.
+ */
+//static const SPIFlash_Device_t my_flash_devices[] = {
+//    W25Q80DLX_EXAMPLE,
+//};
+/*
+ * Specify the number of different devices that are listed in the array we just created. If you add more devices to the array, update this value to match.
+ */
+//const int flashDevices = 1;
+
+
+#include <bluefruit.h>
+void setup()
 {
-  Serial.begin(115200);
-
-  while (!Serial) delay(10);  // wait for serial port to open!
-
-  Serial.println("Orientation Sensor Test"); Serial.println("");
-
-  /* Initialise the sensor */
-  if (!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while (1);
+  flash.begin();
+  Bluefruit.begin(); 
+  if(flash.deepPowerDown() == false){
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    while(1)
+    {
+      yield();
+    }
   }
+  flash.end();
 
-  delay(1000);
+  sd_power_system_off(); 
 }
 
-void loop(void)
+void loop()
 {
-  Serial.print("Loop Start");
-  //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
-  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
-  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
-  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
-  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
-  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
-  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
-
-  printEvent(&orientationData);
-  printEvent(&angVelocityData);
-  printEvent(&linearAccelData);
-  printEvent(&magnetometerData);
-  printEvent(&accelerometerData);
-  printEvent(&gravityData);
-
-  int8_t boardTemp = bno.getTemp();
-  Serial.println();
-  Serial.print(F("temperature: "));
-  Serial.println(boardTemp);
-
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.println();
-  Serial.print("Calibration: Sys=");
-  Serial.print(system);
-  Serial.print(" Gyro=");
-  Serial.print(gyro);
-  Serial.print(" Accel=");
-  Serial.print(accel);
-  Serial.print(" Mag=");
-  Serial.println(mag);
-
-  Serial.println("--");
-  delay(BNO055_SAMPLERATE_DELAY_MS);
-  Serial.print("Loop End");
+  // nothing to do
 }
-
-void printEvent(sensors_event_t* event) {
-  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
-  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
-    Serial.print("Accl:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else if (event->type == SENSOR_TYPE_ORIENTATION) {
-    Serial.print("Orient:");
-    x = event->orientation.x;
-    y = event->orientation.y;
-    z = event->orientation.z;
-  }
-  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
-    Serial.print("Mag:");
-    x = event->magnetic.x;
-    y = event->magnetic.y;
-    z = event->magnetic.z;
-  }
-  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
-    Serial.print("Gyro:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  }
-  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
-    Serial.print("Rot:");
-    x = event->gyro.x;
-    y = event->gyro.y;
-    z = event->gyro.z;
-  }
-  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
-    Serial.print("Linear:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else if (event->type == SENSOR_TYPE_GRAVITY) {
-    Serial.print("Gravity:");
-    x = event->acceleration.x;
-    y = event->acceleration.y;
-    z = event->acceleration.z;
-  }
-  else {
-    Serial.print("Unk:");
-  }
-
-  Serial.print("\tx= ");
-  Serial.print(x);
-  Serial.print(" |\ty= ");
-  Serial.print(y);
-  Serial.print(" |\tz= ");
-  Serial.println(z);
-}
-
-
